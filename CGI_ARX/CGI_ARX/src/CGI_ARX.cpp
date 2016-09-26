d1ad88b6ad52f71359b2b33997f5c5a3d86ad7e7
mgj242 <martin.jurecko@fpt.sk>
2016-09-26 14:43:57 +0200

    Added implementation of vector:toString(), added few demonstration tests to main().

--- CGI_ARX/CGI_ARX/src/CGI_ARX.cpp
+++ CGI_ARX/CGI_ARX/src/CGI_ARX.cpp
@@ -1,7 +1,10 @@
-﻿#include "vector.hpp"
+﻿#include <iostream>
+
+#include "vector.hpp"
 
 
 using namespace CGI_ARX;
+using namespace std;
 
 
 int main() {
@@ -13,6 +16,15 @@ int main() {
 	Vector v3 = createVector(3);
 
 	Vector vysledok = crossProduct(add(v1, v2, 3), v3, 3); // ~ (v1 + v2) x v3
+
+	char buffer[100];
+	if (!toString(v1, 3, buffer, 20))
+		cout << "whoa, malo miesta!" << endl;
+	cout << "toString(v1, ..., 20) == '" << buffer << "'" << endl; // "1.0 2.0 3.0 "
+
+	if (!toString(v1, 3, buffer, 8))
+		cout << "ano, malo miesta" << endl;
+	cout << "toString(v1, ..., 8) == '" << buffer << "'" << endl; // "1.0 2.0 "
 
     return 0;
 }
--- CGI_ARX/CGI_ARX/src/vector.cpp
+++ CGI_ARX/CGI_ARX/src/vector.cpp
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <cstdio>
+#include <cstring>
 
 #include "vector.hpp"
 
@@ -36,11 +38,21 @@ CGI_ARX::Scalar CGI_ARX::norm(const Vector, size_t length) {
 // serialization and deserialization
 
 bool CGI_ARX::toString(Vector vector, size_t vectorLength, char *result, size_t resultLength) {
+	assert(vectorLength > 0);
 	assert(resultLength > 0);
-
-	return false;
+	size_t resultIx = 0;
+	for (size_t ix = 0; ix < vectorLength; ++ix) {
+		const Scalar scalar = vector[ix];
+		size_t volneMiesto = resultLength - resultIx;
+		size_t zapisanychBajtov = snprintf(&result[resultIx], volneMiesto, "%g ", scalar);
+		if (volneMiesto == zapisanychBajtov)
+  		   return ix == vectorLength - 1;
+		resultIx += zapisanychBajtov;
+	}
+	return true;
 }
 
 CGI_ARX::Vector CGI_ARX::fromString(const char *) {
 	return NULL;
 }
+
